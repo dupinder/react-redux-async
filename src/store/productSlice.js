038827ae0c4c1ffd6d38b10462cf3d9467bec4ab
Dupinder <dupinderdhiman5@gmail.com>
2024-08-06 19:19:46 +0530

    Initial commit

---
+++ src/store/productSlice.js
@@ -0,0 +1,2 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initProductState = { products: [{product_id: 0, product_count: 0, product_price: 0}], cart: [] };
---
+++ src/store/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+import uiReducer from "./ui-slice";
+import cartReducer from "./cartSlice";
+
+const store = configureStore({reducer: {ui: uiReducer, cart: cartReducer}});
+export default store;
